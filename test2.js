bb5309697375a0bbf3460a8faf5c615f3bb663b0
RitishBandi30 <S525701@mail.nwmissouri.edu>
2017-08-29 20:16:08 -0400

    Update test2.js

--- test2.js
+++ test2.js
@@ -11,15 +11,15 @@ var count = 4;
 function foo() {
   var table = [count];
   while (count-- > 0) {
-    table.push([]);
+    table.push([count]);// Passing count into table
   }
-  for (var count = 1; count < table.length; count++) {
-    table[count].push(count);
+  for (var count1 = 1; count1 < table.length; count1++) { 
+    table[count1] = [table[count1][0] + (count+2)];// 
   }
   return table;
 }
 
-// console.log(foo());
+console.log(foo());
 
 ////////
 // OR //
@@ -29,13 +29,23 @@ function foo() {
  * The desired result is a countdown from 5 to 0 using console log messages. 
  * Explain why the code only logs -1, then fix the code so it works as expected.
  */
+/*
+The code returns -1 since the value of i is incremented to 6 by the time 
+the callback function in setTimeout is called. To avoid this we call setTimeout
+for every iteration of FOR loop.
 
+*/
 function countdown (num) {
-  for (var i = 0; i <= num; i += 1) {
-    setTimeout(function () {
-      console.log(num - i);
-    }, i * 1000);
+  
+    for (var i = 0; i <= num; i += 1) {  
+    function result(i) {
+        setTimeout(function () {
+            console.log(num-i);
+        }, i * 1000);
+    }
+    result(i);
   }
 }
+
 
 // countdown(5);
